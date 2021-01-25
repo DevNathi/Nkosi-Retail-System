d1e98b5c530904e05d73dc4fb7e60ad1d1594d1c
Nkosinathi Nkosi <nkosinathi.vodacom@gmail.com>
2021-01-25 15:22:51 +0200

    #1 - Added a WPF project for the UI #2 - configured MVVM using Caliburn and Mirco(Nuget package)

---
+++ NK-DesktopUI/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using NK_DesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NK_DesktopUI
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
